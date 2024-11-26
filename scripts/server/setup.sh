0a1a218dc9b923fa5cb027da5a69eb3ddf6199f0
yappy <superunyo@yahoo.co.jp>
2024-11-27 00:36:59 +0900

    Make a backup before --gen-config

--- scripts/server/setup.sh
+++ scripts/server/setup.sh
@@ -4,6 +4,12 @@ SELF_DIR=$(dirname "$(realpath "$0")")
 ELF=${SELF_DIR}/../../server/target/release/bkupserver
 CONFIG=${SELF_DIR}/config.toml
 
+
+if [ -e "${CONFIG}" ]; then
+    set -x
+    mv -i "${CONFIG}" "${CONFIG}~"
+    set +x
+fi
 set -x
 ${ELF} --gen-config "${CONFIG}"
 set +x
