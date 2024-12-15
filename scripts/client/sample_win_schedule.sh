bce64caa0766bb6f26d3653745c9b664b260f693
yappy <superunyo@yahoo.co.jp>
2024-12-15 23:51:40 +0900

    Add windows task scheduler sample

---
+++ scripts/client/sample_win_schedule.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -xue
+
+# Register sample_wsl.sh on Windows TaskScheduler.
+
+# !!! REPLACE HERE !!!
+TASK_NAME="backup\\win"
+TASK_CMD="sample_win.sh"
+# Every month, day, time
+TASK_OPT="/SC MONTHLY /D 2 /ST 04:00"
+# !!! REPLACE HERE !!!
+
+SELF_DIR=$(dirname "$(realpath "$0")")
+
+# use TASK_OPT as is
+# shellcheck disable=SC2086
+schtasks.exe /Create /F /TN ${TASK_NAME} ${TASK_OPT} /TR "wsl.exe --cd ~ -e ${SELF_DIR}/${TASK_CMD}"
+
+echo Notice!
+echo Win+R \"taskschd.msc\" to change more detailed settings.
