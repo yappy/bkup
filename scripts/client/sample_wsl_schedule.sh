8d213d9c18843fc6523351a1b49d7395f731547c
yappy <superunyo@yahoo.co.jp>
2025-04-30 20:53:14 +0900

    Improve sample_wsl_schedule.sh

--- scripts/client/sample_wsl_schedule.sh
+++ scripts/client/sample_wsl_schedule.sh
@@ -1,15 +1,20 @@
-#!/bin/sh -xue
+#!/bin/bash -xue
 
 # Register sample_wsl.sh on Windows TaskScheduler.
 
-# !!! REPLACE HERE !!!
-TASK_NAME="backup\\wsl"
-TASK_CMD="sample_wsl.sh"
-# Every month, day, time
-TASK_OPT="/SC MONTHLY /D 1 /ST 04:00"
-# !!! REPLACE HERE !!!
-
 SELF_DIR=$(dirname "$(realpath "$0")")
+
+DEF_TASK_NAME="backup\\wsl"
+DEF_TASK_CMD="sample_wsl.sh"
+# Every month, day, time
+DEF_TASK_OPT="/SC MONTHLY /D 1 /ST 04:00"
+
+read -r -p "Task Name (default: ${DEF_TASK_NAME}): " TASK_NAME
+read -r -p "Task Command (default: ${DEF_TASK_CMD}): " TASK_CMD
+read -r -p "Task Options (default: ${DEF_TASK_OPT}): " TASK_OPT
+echo "${TASK_NAME:=${DEF_TASK_NAME}}"
+echo "${TASK_CMD:=${DEF_TASK_CMD}}"
+echo "${TASK_OPT:=${DEF_TASK_OPT}}"
 
 # use TASK_OPT as is
 # shellcheck disable=SC2086
