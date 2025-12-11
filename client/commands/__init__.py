964b7531cf0fbd1090a5b578f26f48c95ec37b11
yappy <superunyo@yahoo.co.jp>
2025-12-11 14:43:03 +0900

    Add rclone wrapper into python scripts

--- client/commands/__init__.py
+++ client/commands/__init__.py
@@ -1,8 +1,10 @@
-from . import sync, archive, clean, upload
+from . import sync, archive, clean, upload, cloudsetup, cloud
 
 command_table = [
     (sync.main, "sync", "Make a backup copy of directory"),
     (clean.main, "clean", "Clean old archive files"),
     (archive.main, "archive", "Compress directory and make a archive file"),
     (upload.main, "upload", "Copy the latest archive file to a remote host by rsync"),
+    (cloudsetup.main, "cloudsetup", "Setup rclone tool"),
+    (cloud.main, "cloud", "Copy the latest archive file to a cloud storage"),
 ]
---
+++ client/commands/cloudsetup.py
@@ -0,0 +1,79 @@
+import logging
+import argparse
+import subprocess
+import platform
+
+log = logging.getLogger(__name__)
+
+
+def get_current_version() -> str | None:
+    try:
+        proc = subprocess.run(
+            ["rclone", "version"],
+            check=True, text=True, stdout=subprocess.PIPE, stderr=None)
+        tokens = proc.stdout.split()
+        assert len(tokens) >= 2
+        assert tokens[0] == "rclone"
+        assert tokens[1].startswith("v")
+        return tokens[1]
+    except Exception as e:
+        log.warning("rclone version failed")
+        log.warning(e)
+        return None
+
+
+def get_latest_version() -> str:
+    URL = "https://downloads.rclone.org/version.txt"
+
+    log.info(f"Download: {URL}")
+    proc = subprocess.run(
+        ["wget", "-q", "-O", "-", URL],
+        check=True, text=True, stdout=subprocess.PIPE, stderr=None)
+    tokens = proc.stdout.split()
+    assert tokens[0] == "rclone"
+    assert tokens[1].startswith("v")
+    return tokens[1]
+
+
+def compare_version(v1: str, v2: str) -> int:
+    def parse(ver: str) -> tuple[int, int, int]:
+        assert ver.startswith("v")
+        nums = list(map(int, ver[1:].split(".")))
+        assert len(nums) == 3
+        return nums[0], nums[1], nums[2]
+
+    v1 = parse(v1)
+    v2 = parse(v2)
+    if v1[0] != v2[0]:
+        return v1[0] - v2[0]
+    elif v1[1] != v2[1]:
+        return v1[1] - v2[1]
+    else:
+        return v1[2] - v2[2]
+
+
+def cloudsetup(args: argparse.Namespace):
+    cur = get_current_version()
+    if cur is not None:
+        log.info(f"Current rclone: {cur}")
+    else:
+        log.info("rclone is not installed?")
+
+    latest = get_latest_version()
+    log.info(f"Latest rclone: {latest}")
+
+    if cur is None or compare_version(cur, latest) >= 0:
+        return
+    log.info("Install the latest rclone!")
+
+
+def main(argv: list[str]):
+    parser = argparse.ArgumentParser(
+        prog=argv[0],
+        description="Setup rclone tool",
+        epilog="This is rclone wrapper",
+    )
+
+    args = parser.parse_args(argv[1:])
+
+    cloudsetup(args)
